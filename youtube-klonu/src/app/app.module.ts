05041abcdd91a4375bfc8dc03102353076f57af5
Metinaba1905 <abametin722@gmail.com>
2023-06-29 17:51:40 +0300

    şuan için gerekli componentler oluşturuldu

--- youtube-klonu/src/app/app.module.ts
+++ youtube-klonu/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SearchComponent } from './search/search.component';
+import { VideoDetailsComponent } from './video-details/video-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SidebarComponent,
+    SearchComponent,
+    VideoDetailsComponent
   ],
   imports: [
     BrowserModule,
